3513764d976e0bf042c0d01fb511db94acf7b832
Levente Kis <kis.levente@edu.bme.hu>
2021-06-26 16:01:35 +0200

    Update main.m

--- main.m
+++ main.m
@@ -5,7 +5,7 @@ close all;
 INTERPMODE = "BARYC"; % Interpolálási mód kiválasztása. "BARYC" vagy "GRAF"
 STLNAME = "hangvilla.stl"; % STL file neve
 ZOOM = 4.3e-003; % Mekkora nagyításban lássuk az elmozdulást?
-MODE = 3; % Hanyadik sajátfrekvenciához tartozó lengésképet jelenítsük meg?
+MODE = 1; % Hanyadik sajátfrekvenciához tartozó lengésképet jelenítsük meg?
 REFINE = [0.01 1];  % Mennyire sűrítsük az eredeti STL hálót? (első elem: maximálisan megengedett élhossz;
 %                                                              második elem: az első elemnél nagyobb, tetszőlegesen választott szám)
 
